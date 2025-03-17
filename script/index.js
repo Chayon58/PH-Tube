e4e2785a597775b26148959707bda352af707aeb
Chayon58 <chayonbiswas8610@gmail.com>
2025-03-17 20:47:00 +0600

    added hover

--- script/index.js
+++ script/index.js
@@ -4,6 +4,16 @@ function loadCategories(){
     .then((data)=> displayCategories(data.categories))
 }
 function displayCategories(categories){
-    console.log(categories);
+   const categoryContainer = document.getElementById("category-container");
+   for(let cat of categories){
+    console.log(cat);
+
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML =`
+    <button class="btn btn-sm hover:bg-[red] hover:text-white">${cat.category}</button>
+    `;
+
+    categoryContainer.append(categoryDiv)
+   }
 }
 loadCategories();
