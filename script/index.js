50245524fb95d4e23f6aeb750dad972686f59f2d
Chayon58 <chayonbiswas8610@gmail.com>
2025-03-18 22:27:34 +0600

    added video container

--- script/index.js
+++ script/index.js
@@ -3,10 +3,19 @@ function loadCategories(){
     .then((res)=>res.json())
     .then((data)=> displayCategories(data.categories))
 }
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res)=>res.json())
+    .then((data)=> displayVideo(data.videos))
+}
+
+
+
 function displayCategories(categories){
    const categoryContainer = document.getElementById("category-container");
    for(let cat of categories){
-    console.log(cat);
+    // console.log(cat);
 
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML =`
@@ -16,4 +25,15 @@ function displayCategories(categories){
     categoryContainer.append(categoryDiv)
    }
 }
+
+const displayVideo = (videos) =>{
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach((video) => {
+        console.log(videos);
+    })
+}
+
+
 loadCategories();
+loadVideos();
